278ade3137144eb962096fadde8c1c8e2c1127b5
Romain Santiard <romainsantiard@gmail.com>
2025-12-19 16:21:36 +0100

    actu5

--- src/run_me.m
+++ src/run_me.m
@@ -1,8 +1,8 @@
-% run_me.m - Version minimaliste
-fprintf('1 - Launch the stress propagation animation (seuil_anim.m)\n');
-fprintf('2 - Plot the graph to estimate the critic mass value (seuil_graph.m)\n');
-fprintf('3 - Launch the escape animation (escape_anim.m)\n');
-fprintf('4 - Plot the graph time to escape a room vs stress level of the crowd (escape_graph.m)\n');
+% run_me.m
+fprintf('1 - seuil_anim.m\n');
+fprintf('2 - seuil_graph.m\n');
+fprintf('3 - escape_anim.m\n');
+fprintf('4 - escape_graph.m\n');
 
 choix = input('Entrez 1,2,3 ou 4 : ');
 
@@ -11,17 +11,22 @@ switch choix
         seuil_anim();
         
     case 2
-        % Compiler et exécuter les fichiers C
-        system('cd ../data && gcc -o thresholds_matrix thresholds_matrix.c -lm');
-        system('cd ../data && gcc -o state_matrix state_matrix.c -lm');
-        system('cd ../data && ./thresholds_matrix');
-        system('cd ../data && ./state_matrix');
+        % ÉTAPE 1: Compiler les fichiers C vers bin/
+        fprintf('Compilation des fichiers C...\n');
+        system('gcc -o ../bin/thresholds_matrix thresholds_matrix.c -lm');
+        system('gcc -o ../bin/state_matrix state_matrix.c -lm');
         
-        % Lancer la simulation
+        % ÉTAPE 2: Exécuter depuis bin/
+        fprintf('Génération des fichiers binaires...\n');
+        system('cd ../bin && ./thresholds_matrix');
+        system('cd ../bin && ./state_matrix');
+        
+        % ÉTAPE 3: Lancer la simulation
         seuil_graph();
         
-        % Sauvegarder
+        % ÉTAPE 4: Sauvegarder dans results/
         saveas(gcf, '../results/escape_graph.png');
+        fprintf('Graphique sauvegardé dans results/escape_graph.png\n');
         
     case 3
         escape_anim();
